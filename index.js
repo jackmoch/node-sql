29c4eb60407bae4bf0eedf24c7b234edcd8099cd
Jack Mocherman <mochermanjack@gmail.com>
2016-10-12 11:14:55 -0500

    output table to terminal using cli-table package

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const { Database } = require('sqlite3').verbose()
+const Table = require('cli-table')
+
+const db = new Database('db/Chinook_Sqlite.sqlite')
+
+db.serialize(() => {
+	// db.all(`
+	// 	SELECT Customer.FirstName || " " || Customer.LastName AS "Name", 
+	// 	       Customer.CustomerId,
+	// 	       Customer.Country 
+	// 	FROM   Customer
+	// 	WHERE  Customer.Country != "USA"
+	// `, (err, customers) => {
+	// 	console.log(customers)
+	// })
+
+	// db.all(`
+	// 	SELECT Customer.FirstName || " " || Customer.LastName AS "Name", 
+	// 	       Customer.CustomerId,
+	// 	       Customer.Country 
+	// 	FROM   Customer
+	// 	WHERE  Customer.Country = "Brazil"
+	// `, (err, customers) => {
+	// 	customers.forEach((customer) => {
+	// 		console.log(`${customer.CustomerId}: ${customer.Name} (${customer.Country})`)
+	// 	})
+	// })
+
+	// db.each(`
+	// 	SELECT Customer.FirstName || " " || Customer.LastName AS "Name", 
+	// 	       Customer.CustomerId,
+	// 	       Customer.Country 
+	// 	FROM   Customer
+	// 	WHERE  Customer.Country = "Brazil"
+	// `, (err, {CustomerId, Name, Country}) => {
+	// 	console.log(`${CustomerId}: ${Name} (${Country})`)
+	// })
+
+	db.each(`
+		SELECT Customer.FirstName || " " || Customer.LastName as "Name",
+		       Invoice.InvoiceId,
+		       Invoice.InvoiceDate, 
+		       Invoice.BillingCountry from Customer
+		JOIN   Invoice on Customer.CustomerId = Invoice.CustomerId
+		WHERE  Customer.Country = "Brazil"
+	`, (err, {InvoiceId, Name, InvoiceDate, BillingCountry}) => {
+		console.log(`${InvoiceId}: ${Name}, ${InvoiceDate} (${BillingCountry})`)
+	})
+
+	var table = new Table({
+    head: ['Invoice Id', 'Name', 'Invoice Date', 'Billing Country'], 
+    colWidths: [30, 30, 30, 30],
+    style: {compact: true}
+	});
+
+	db.each(`
+		SELECT Customer.FirstName || " " || Customer.LastName as "Name",
+		       Invoice.InvoiceId,
+		       Invoice.InvoiceDate, 
+		       Invoice.BillingCountry from Customer
+		JOIN   Invoice on Customer.CustomerId = Invoice.CustomerId
+		WHERE  Customer.Country = "Brazil"
+	`, (err, { Name, InvoiceId, InvoiceDate, BillingCountry }) => {
+		table.push(
+			[`${InvoiceId}`, `${Name}`, `${InvoiceDate}`, `${BillingCountry}`]
+		)
+	}, () => {
+		console.log(table.toString());
+	})
+
+})
+
+db.close()
